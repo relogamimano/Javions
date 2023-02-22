b2c5fec98763440a58550d8d9daff60531a9e5c5
relogamimano <romeo.maignal@epfl.ch>
2023-02-22 17:42:22 +0100

    archizoom

--- src/ch/epfl/javions/Units.java
+++ src/ch/epfl/javions/Units.java
@@ -2,7 +2,7 @@ package ch.epfl.javions;
 
 public final class Units {
     private Units() {}
-
+    //adfadasdadgsadg
     public static final double CENTI = 1e-2;
     public static final double KILO = 1e3;
     // … autre(s) préfixe(s) SI utile(s) au projet
