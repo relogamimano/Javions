c8b961fd977a475edcbb64c2417110b876cec219
relogamimano <119960834+relogamimano@users.noreply.github.com>
2023-03-08 11:08:16 +0100

    Update SamplesDecoder.java

--- src/ch/epfl/javions/demodulation/SamplesDecoder.java
+++ src/ch/epfl/javions/demodulation/SamplesDecoder.java
@@ -7,37 +7,38 @@ import java.io.InputStream;
 
 /**
  * @author: Sofia Henriques Garfo sciper:
+ * @author: Roméo Maignal sciper:
  */
 public class SamplesDecoder {
-    private byte[] sample;
+    private byte[] sampleTab;
     private InputStream sampleStream;
     private int byteNumber;
-
 
     public SamplesDecoder(InputStream stream, int batchSize){
         Preconditions.checkArgument(batchSize > 0);
         if ( stream == null) {
             throw new NullPointerException();
         }
-        sample = new byte[batchSize*2];
-        byteNumber = batchSize*2;
+        byteNumber = batchSize * 2;
+        sampleTab = new byte[byteNumber];
         sampleStream = stream;
 
     }
 
     public int readBatch(short[] batch) throws IOException {
-        int readBytes = sampleStream.readNBytes(sample, 0,byteNumber);
-        Preconditions.checkArgument(batch.length == byteNumber );
+        // TODO: 08.03.23 voir powercomputer pour l'emplacement de l'appel a un flot 
+        int readBytes = sampleStream.readNBytes(sampleTab, 0, byteNumber);
+        Preconditions.checkArgument(batch.length == byteNumber/2);
         if ( readBytes != byteNumber ){
             byteNumber = (int) Math.floor( readBytes/2);
         }
 
-        for (int i=0;i<=(byteNumber-1); i++ ){
-            int j = i+1;
-            int mostSignificantBits = (sample[j] & 0xf);
-            int leastSignificantBits = sample[0];
-            batch[i] = (short) (leastSignificantBits + ( mostSignificantBits << 8 ));
-
+        for (int i = 0; i < batch.length; i++ ) {
+            int j = i*2;
+            short mostSignificantBits = (short)(sampleTab[j + 1] & 0xf);
+            short leastSignificantBits = sampleTab[j] ;
+            short uncenteredSample = (short) ((mostSignificantBits << 8)  + (leastSignificantBits & 0xff));
+            batch[i] = (short)(uncenteredSample - (short)2048);
         }
         return readBytes/2;
     }
