f0d21032e8a44ae197bb3377f54e8b8f752153f6
relogamimano <119960834+relogamimano@users.noreply.github.com>
2023-03-09 13:34:34 +0100

    Update PowerComputerTest.java

--- test/ch/epfl/javions/demodulationTest/PowerComputerTest.java
+++ test/ch/epfl/javions/demodulationTest/PowerComputerTest.java
@@ -1,22 +1,38 @@
 package ch.epfl.javions.demodulationTest;
 
 import ch.epfl.javions.demodulation.PowerComputer;
-import ch.epfl.javions.demodulation.SamplesDecoder;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PowerComputerTest {
     @Test
     void readBatchWorksWithTrivialValues() throws IOException {
         DataInputStream stream = new DataInputStream(new BufferedInputStream(
                 new FileInputStream(new File("resources/samples.bin"))));
-        PowerComputer powerComputer = new PowerComputer(stream, 10);
+        // TODO: 09.03.23 batchSize c'est la taille du nb de byte qu'il faut sortir du stream ou c'est ce meme flot divisé par 4 (le nb de puissance que l'on pourra compooser) ? 
+        PowerComputer powerComputer = new PowerComputer(stream, 80);
         int [] expected = {73, 292, 65, 745, 98, 4226, 12244, 25722, 36818, 23825};
-        int [] batch = new int[10];
+        int [] batch = new int[80];
         powerComputer.readBatch(batch);
-        assertEquals(expected,batch);
+        assertArrayEquals(expected, Arrays.copyOf(batch, 10));
+    }
+    @Test
+    void constructorThrowsIllegalArgumentException() throws IOException {
+        DataInputStream stream = new DataInputStream(new BufferedInputStream(
+                new FileInputStream(new File("resources/samples.bin"))));
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 79));
+    }
+    @Test
+    void readBatchThrowsIllegalArgumentException() throws IOException {
+        DataInputStream stream = new DataInputStream(new BufferedInputStream(
+                new FileInputStream(new File("resources/samples.bin"))));
+        int [] batch = new int[41];
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 80).readBatch(batch));
+
+
     }
 }
