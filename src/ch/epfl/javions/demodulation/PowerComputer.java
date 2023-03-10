5628d9d2708d95796702a998f47877f1c1db07cd
relogamimano <119960834+relogamimano@users.noreply.github.com>
2023-03-10 15:08:40 +0100

    Update PowerComputer.java

--- src/ch/epfl/javions/demodulation/PowerComputer.java
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -1,18 +1,18 @@
 package ch.epfl.javions.demodulation;
 
-import ch.epfl.javions.Bits;
 import ch.epfl.javions.Preconditions;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * @author: Sofia Henriques Garfo sciper:
- * @author: Roméo Maignal sciper:
+ * * @author: Sofia Henriques Garfo (346298)
+ *  * @author: Romeo Maignal (360568)
  */
 public final class PowerComputer {
     private final short[] sample;
-    private int[] queue = new int[Bits.LENTGH];
+    private int[] queue = new int[Byte.SIZE];
+
     private final int sampleNumber;
     private final InputStream sampleStream;
     // TODO: 08.03.23 readSample comme ca ou plutot sur le modele de readByte comme pour SampleDecoder ?
@@ -25,11 +25,10 @@ public final class PowerComputer {
      * @param batchSize length that determine how many bytes, from the input stream, will be used
      */
     public PowerComputer(InputStream stream, int batchSize) {
-        Preconditions.checkArgument((batchSize % Bits.LENTGH == 0) && (batchSize > 0));
+        Preconditions.checkArgument((batchSize % Byte.SIZE == 0) && (batchSize > 0));
         sampleNumber = batchSize * 2;
         sampleStream = stream;
         sample = new short[sampleNumber];
-
     }
 
     /**
@@ -44,12 +43,12 @@ public final class PowerComputer {
         Preconditions.checkArgument(batch.length == sampleNumber/2);
         int readSample = samplesDecoder.readBatch(sample);
 
-
-        for (int i = 0; i < batch.length; i++) {
+        for (int i = 0; i < readSample / 2; i++) {
             queue = removeFirstTwoAddTwo(queue, sample[i*2], sample[i*2 + 1]);
             int I = 0;
             int Q = 0;
             for (int j = 0,  k = 0; j < queue.length-1 || k < queue.length / 2; j+=2, k++) {
+                // TODO: 10.03.23 changer pow
                 I = I + (int)( Math.pow( -1, k + 1) * queue[j] );
                 Q = Q + (int)( Math.pow( -1, k + 1) * queue[j+1] );
             }
@@ -61,7 +60,7 @@ public final class PowerComputer {
     }
     // methode used to shift the queue to the left two times and add the two next samples at the end
     private int[] removeFirstTwoAddTwo(int[] tab, int v1, int v2) {
-        int[] shiftedTab = new int[Bits.LENTGH];
+        int[] shiftedTab = new int[Byte.SIZE];
         System.arraycopy(tab, 2, shiftedTab, 0, shiftedTab.length - 2);
         shiftedTab[6] = v1;
         shiftedTab[7] = v2;
