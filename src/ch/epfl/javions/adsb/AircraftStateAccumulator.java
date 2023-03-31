b30a0171aaae4529286d935d5ee2a8523808ade0
relogamimano <119960834+relogamimano@users.noreply.github.com>
2023-03-31 15:01:01 +0200

    c terminé wesh

--- src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
+++ src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
@@ -2,9 +2,10 @@ package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.Preconditions;
+import ch.epfl.javions.Units;
 
 public class AircraftStateAccumulator<T extends AircraftStateSetter>  {
-    private static final long NANO_TO_SEC = 10_000_000_000L;
+    private static final long TIMESTAMP_DIFF = 10;
     private T stateSetter;
     public AircraftStateAccumulator(T stateSetter) throws NullPointerException {
         Preconditions.checkArgument(stateSetter != null);
@@ -15,6 +16,11 @@ public class AircraftStateAccumulator<T extends AircraftStateSetter>  {
     }
     private AirbornePositionMessage lastOddPositionMessage;
     private AirbornePositionMessage lastEvenPositionMessage;
+
+    /**
+     *
+     * @param message
+     */
     public void update(Message message) {
         stateSetter.setLastMessageTimeStampNs(message.timeStampNs());
         switch (message) {
@@ -29,18 +35,14 @@ public class AircraftStateAccumulator<T extends AircraftStateSetter>  {
                 } else {
                     lastOddPositionMessage = aim;
                 }
-                if (lastOddPositionMessage != null
-                        && lastEvenPositionMessage != null
-                        && Math.abs(lastEvenPositionMessage.timeStampNs() - lastOddPositionMessage.timeStampNs() ) <= NANO_TO_SEC) {
-                    GeoPos m = CprDecoder.decodePosition(lastEvenPositionMessage.x(),
-                            lastEvenPositionMessage.y(), lastOddPositionMessage.x(),
+                if (lastOddPositionMessage != null && lastEvenPositionMessage != null
+                        && Math.abs(lastEvenPositionMessage.timeStampNs() - lastOddPositionMessage.timeStampNs() ) <= Units.convertTo(TIMESTAMP_DIFF, Units.NANO)) {
+                    GeoPos m = CprDecoder.decodePosition(lastEvenPositionMessage.x(), lastEvenPositionMessage.y(), lastOddPositionMessage.x(),
                             lastOddPositionMessage.y(), aim.parity());
                     if (m != null) {
                         stateSetter.setPosition(m);
                     }
-
                 }
-
             }
             case AirborneVelocityMessage aim -> {
                 stateSetter.setVelocity(aim.speed());
