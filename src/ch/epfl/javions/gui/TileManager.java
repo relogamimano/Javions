49450262a66ba56ae937a8f0dd74b2ba3bd383de
relogamimano <romeo.maignal@epfl.ch>
2023-05-03 14:35:34 +0200

    TileManager done with javadoc

--- src/ch/epfl/javions/gui/TileManager.java
+++ src/ch/epfl/javions/gui/TileManager.java
@@ -11,6 +11,11 @@ import java.util.LinkedHashMap;
 
 import static ch.epfl.javions.Preconditions.*;
 
+/**
+ * OSM tile manager
+ * @author Sofia Henriques Garfo (346298)
+ * @author Romeo Maignal (360568)
+ */
 public final class TileManager {
     private static final int MAX_CAPACITY = 100;
     private static final float LOAD_FACTOR = 0.75f;
@@ -19,7 +24,20 @@ public final class TileManager {
     private final String serverAddress;
     private final LinkedHashMap<TileId, Image> memoryCache = new LinkedHashMap<>(MAX_CAPACITY, LOAD_FACTOR, LRU_ORDER);
 
+    /**
+     * Tile identity record
+     * @param zoom  level of magnification
+     * @param x     abscissa index of the tile
+     * @param y     ordinate index of the tile
+     */
     public record TileId(int zoom, int x, int y) {
+        /**
+         * Methode to check if the tile attributes are valid
+         * @param zoom level of magnification
+         * @param x    abscissa index of the tile
+         * @param y    ordinate index of the tile
+         * @return  validity of the tile
+         */
         public static boolean isValid(int zoom, int x, int y) {
             return ( 6 <= zoom && zoom <= 19 )
                     && ( 0 <= x && x <= ( 1 << ( zoom + 1 ) ) )
@@ -27,11 +45,23 @@ public final class TileManager {
         }
     }
 
+    /**
+     * Constructor of TileManager
+     * @param filePath  UNIX Path of the disc cache directory
+     * @param serverAddress     online server address
+     */
     public TileManager(Path filePath, String serverAddress) {
         this.discCachePath = filePath;
         this.serverAddress = serverAddress;
     }
 
+    /**
+     * Image getter method : it takes a specific tile ID and look it up in the different image storing caches,
+     * or download it from an online server.
+     * @param tileId    Tile ID
+     * @return  tile Image
+     * @throws IOException if the input reading or the output writing fails
+     */
     public Image imageForTileAt(TileId tileId) throws IOException {
         checkArgument(TileId.isValid(tileId.zoom(), tileId.x(), tileId.y()));
 
