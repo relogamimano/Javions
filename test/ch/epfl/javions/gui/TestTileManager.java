a89affa02b911ca9b6683c1f2a999e99d6448a5f
relogamimano <romeo.maignal@epfl.ch>
2023-05-05 15:38:06 +0200

    test

---
+++ test/ch/epfl/javions/gui/TestTileManager.java
@@ -0,0 +1,25 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.gui.TileManager;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import java.nio.file.Path;
+
+public final class TestTileManager extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        TileManager t = new TileManager(Path.of("resources/tile-cache"),
+                "tile.openstreetmap.org");
+        t.imageForTileAt(new TileManager.TileId(17, 67927, 46357));
+        t.imageForTileAt(new TileManager.TileId(17, 67927, 46350));
+        for (int i = 0; i < 100; i++) {
+            t.imageForTileAt(new TileManager.TileId(17, 67927, 46357 + i));
+        }
+//        t.imageForTileAt(new TileManager.TileId(17, 67927, 46350));
+        Platform.exit();
+    }
+}
