a32797f3455fa87a10679e99bcbdf01887ef84e8
Sofia Garfo <41451008+aihxpos@users.noreply.github.com>
2023-05-10 17:26:39 +0200

    Update MapParameters.java

--- src/ch/epfl/javions/gui/MapParameters.java
+++ src/ch/epfl/javions/gui/MapParameters.java
@@ -1,4 +1,4 @@
-package ch.epfl.javions.gui;
+ppackage ch.epfl.javions.gui;
 
 import ch.epfl.javions.Math2;
 import ch.epfl.javions.Preconditions;
@@ -7,17 +7,18 @@ import javafx.beans.property.*;
 public final class MapParameters {
 
     private static final int MAX_ZOOM = 19;
-    private static final int MIN_ZOOM = 6 ;
+    private static final int MIN_ZOOM = 6;
     private IntegerProperty zoom = new SimpleIntegerProperty(); // zoom level , 6 <= zoom level <= 19
     private DoubleProperty minX = new SimpleDoubleProperty(); // x value of the left hand top corner
     private DoubleProperty minY = new SimpleDoubleProperty(); // y value of the top left hand corner
 
-    public MapParameters(int zoom, double minX, double minY){
+    public MapParameters(int zoom, double minX, double minY) {
         Preconditions.checkArgument(zoom >= MIN_ZOOM && zoom <= MAX_ZOOM);
         this.zoom.set(zoom);
         this.minX.set(minX);
         this.minY.set(minY);
     }
+
     public ReadOnlyIntegerProperty zoomProperty() {
         return zoom;
     }
@@ -29,6 +30,7 @@ public final class MapParameters {
     public ReadOnlyDoubleProperty minXProperty() {
         return minX;
     }
+
     public double getMinX() {
         return this.minX.get();
     }
@@ -41,20 +43,21 @@ public final class MapParameters {
         return this.minY.get();
     }
 
-    public void scroll(int translateX, int translateY){
+    public void scroll(double translateX, double translateY) {
         this.minX.set(this.minX.get() + translateX);
         this.minY.set(this.minY.get() + translateY);
     }
 
 
     public void changeZoomLevel(int deltaZoom) {
+        int oldzoom = this.getZoomLevel();
         zoom.set(Math2.clamp(MIN_ZOOM, this.getZoomLevel() + deltaZoom, MAX_ZOOM));
-        minX.set(Math.scalb(minX.get(), deltaZoom) );
-        minY.set(Math.scalb(minY.get(), deltaZoom) );
+        minX.set(minX.get() * (getZoomLevel()-oldzoom) );
+        minY.set(minY.get() * (getZoomLevel()-oldzoom) );
 
     }
 
 
-
 }
+
 
