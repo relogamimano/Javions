ba58c51457b87ec83020cb57043a0693fa5079ae
relogamimano <romeo.maignal@epfl.ch>
2023-05-27 01:05:47 +0200

    Geopos avec constante

--- src/ch/epfl/javions/GeoPos.java
+++ src/ch/epfl/javions/GeoPos.java
@@ -1,14 +1,18 @@
 package ch.epfl.javions;
 
+import static ch.epfl.javions.Units.Angle.DEGREE;
+import static ch.epfl.javions.Units.Angle.T32;
+import static ch.epfl.javions.Units.convert;
+
 /**
  * Represent the geographical coordinates expressed in T32 and stocked as int
  * @param longitudeT32
  * @param latitudeT32
- * @author: Sofia Henriques Garfo (346298)
- * @author: Romeo Maignal (360568)
+ * @author Sofia Henriques Garfo (346298)
+ * @author Romeo Maignal (360568)
  */
 public record GeoPos(int longitudeT32, int latitudeT32) {
-
+    private static final int MAX_ABSOLUTE_LATITUDE_T32 = 1 << 30;
     /**
      * Checks if the given geographical coordinates are valid
      * @throws IllegalArgumentException if the coordinates aren't valid
@@ -25,7 +29,7 @@ public record GeoPos(int longitudeT32, int latitudeT32) {
      * @return true if the latitude is valid
      */
     public static boolean isValidLatitudeT32(int latitudeT32) {
-        return (latitudeT32 >= (-Math.pow(2, 30))) && (latitudeT32 <= Math.pow(2, 30));
+        return Math.abs(latitudeT32) <= MAX_ABSOLUTE_LATITUDE_T32;
     }
 
     /**
@@ -34,7 +38,7 @@ public record GeoPos(int longitudeT32, int latitudeT32) {
      */
     public double longitude() {
 
-        return Units.convertFrom(longitudeT32, Units.Angle.T32);
+        return Units.convertFrom(longitudeT32, T32);
     }
 
     /**
@@ -43,15 +47,15 @@ public record GeoPos(int longitudeT32, int latitudeT32) {
      */
     public double latitude () {
 
-        return Units.convertFrom(latitudeT32, Units.Angle.T32);
+        return Units.convertFrom(latitudeT32, T32);
     }
 
 
     @Override
     public String toString() {
 
-        double longitudeDEG = Units.convert(longitudeT32, Units.Angle.T32, Units.Angle.DEGREE);
-        double latitudeDEG = Units.convert(latitudeT32, Units.Angle.T32, Units.Angle.DEGREE);
+        double longitudeDEG = convert(longitudeT32, T32, DEGREE);
+        double latitudeDEG = convert(latitudeT32, T32, DEGREE);
 
         return "(" + longitudeDEG + "°, " + latitudeDEG + "°)";
     }
