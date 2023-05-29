aae1a2a0c9beed000f6d8ffc559bfcade5d316d6
relogamimano <romeo.maignal@epfl.ch>
2023-05-30 00:50:23 +0200

    StatusLineController with additional javadoc

--- src/ch/epfl/javions/gui/StatusLineController.java
+++ src/ch/epfl/javions/gui/StatusLineController.java
@@ -1,13 +1,13 @@
 package ch.epfl.javions.gui;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.*;
-import javafx.collections.ObservableSet;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-
+/**
+ * Status Line Controller display the number of visible aircraft and the number of received messages
+ * @author Sofia Henriques Garfo (346298)
+ * @author Romeo Maignal (360568)
+ */
 public final class StatusLineController {
     private final BorderPane pane;
     private final IntegerProperty aircraftCountProperty;
@@ -35,9 +35,22 @@ public final class StatusLineController {
         return rightTxt;
     }
 
-    public BorderPane pane() { return pane;}
+    /**
+     * Getter method used to access the pane
+     * @return (Pane) pane
+     */
+    public BorderPane pane() { return pane; }
 
+    /**
+     * Getter method used to access the modifiable aircraft count property
+     * @return (int) modifiable aircraft count property
+     */
     public IntegerProperty aircraftCountProperty() { return aircraftCountProperty; }
+
+    /**
+     * Getter methode used to access the modifiable message count property
+     * @return (long) modifiable message count property
+     */
 
     public LongProperty messageCountProperty() { return messageCountProperty; }
 }
