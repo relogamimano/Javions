9af6d4b90a01b22cd0b2ee382c09a66996065b81
relogamimano <romeo.maignal@epfl.ch>
2023-05-31 13:56:42 +0200

    StatusLine avec nouvelles constantes

--- src/ch/epfl/javions/gui/StatusLineController.java
+++ src/ch/epfl/javions/gui/StatusLineController.java
@@ -1,10 +1,15 @@
 package ch.epfl.javions.gui;
 
-import javafx.beans.property.*;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+
 /**
  * Status Line Controller display the number of visible aircraft and the number of received messages
+ *
  * @author Sofia Henriques Garfo (346298)
  * @author Romeo Maignal (360568)
  */
@@ -12,45 +17,61 @@ public final class StatusLineController {
     private final BorderPane pane;
     private final IntegerProperty aircraftCountProperty;
     private final LongProperty messageCountProperty;
+    private static final String STATUS_LINE_CSS_FILE = "status.css";
+    private static final String LEFT_REF = "Aéronefs visibles : ";
+    private static final String RIGHT_REF = "Messages reçus : ";
+
     public StatusLineController() {
         this.pane = new BorderPane();
         aircraftCountProperty = new SimpleIntegerProperty();
         messageCountProperty = new SimpleLongProperty();
-        pane.getStylesheets().add("status.css");
+        pane.getStylesheets().add(STATUS_LINE_CSS_FILE);
         pane.setLeft(leftTxt());
         pane.setRight(rightTxt());
     }
+
     private Text leftTxt() {
-        String leftRef = "Aéronefs visibles : ";
         Text leftTxt = new Text();
         leftTxt.textProperty().bind(
-                aircraftCountProperty.map(m -> { return leftRef + m.intValue(); } ));
+                aircraftCountProperty.map(m -> {
+                    return LEFT_REF + m.intValue();
+                }));
         return leftTxt;
     }
+
     private Text rightTxt() {
-        String rightRef = "Messages reçus : ";
         Text rightTxt = new Text();
         rightTxt.textProperty().bind(
-                messageCountProperty.map(m -> { return rightRef + m.longValue(); } ));
+                messageCountProperty.map(m -> {
+                    return RIGHT_REF + m.longValue();
+                }));
         return rightTxt;
     }
 
     /**
      * Getter method used to access the pane
+     *
      * @return (Pane) pane
      */
-    public BorderPane pane() { return pane; }
+    public BorderPane pane() {
+        return pane;
+    }
 
     /**
      * Getter method used to access the modifiable aircraft count property
+     *
      * @return (int) modifiable aircraft count property
      */
-    public IntegerProperty aircraftCountProperty() { return aircraftCountProperty; }
+    public IntegerProperty aircraftCountProperty() {
+        return aircraftCountProperty;
+    }
 
     /**
      * Getter methode used to access the modifiable message count property
+     *
      * @return (long) modifiable message count property
      */
-
-    public LongProperty messageCountProperty() { return messageCountProperty; }
+    public LongProperty messageCountProperty() {
+        return messageCountProperty;
+    }
 }
